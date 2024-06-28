f73b42797be1de7f944e9e2bb8571b0f2fcc7ff2
alda-1995 <aldair@canalla.agency>
2024-06-28 17:21:16 -0600

    se agrega codigo que valida el estatus del token

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,11 +1,25 @@
 import axios from "axios";
+import { useAuthStore } from "@/store/authStore";
 
 axios.interceptors.request.use((config) => {
     const token = JSON.parse(localStorage.getItem('token'));
-    if(token){
+    if (token) {
         config.headers['Authorization'] = `Bearer ${token}`;
     }
     return config;
 });
+
+axios.interceptors.response.use(
+    response => {
+        return response;
+    },
+    error => {
+        if (error.response && error.response.statusText === "Unauthorized") {
+            const authStore = useAuthStore();
+            authStore.logout();
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default axios;
